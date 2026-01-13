8167dfacd40ca8f259e9deea045ec466ba327701
cihr23110 <cihr23110@g.nihon-u.ac.jp>
2026-01-13 10:28:14 +0900

    add runtimeError.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -11,7 +11,10 @@ import java.util.List;
 
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
 
     public static void main(String[] args) throws IOException {
         if(args.length > 1){
@@ -27,6 +30,8 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
     private static void runPrompt() throws IOException {
         InputStreamReader input =new InputStreamReader(System.in);
@@ -51,7 +56,7 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
 
     }
 
@@ -71,6 +76,12 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
-  }
+    }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
+    }
 
 }
